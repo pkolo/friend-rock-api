5757b7ed5e8c385db90d292cc41fc2b7b4792211
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-07 16:58:12 -0500

    Add auth token to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :auth_token, uniqueness: true
+  
 end
