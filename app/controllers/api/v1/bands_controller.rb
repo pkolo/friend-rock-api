158f25287397c4e8bcb39d8c71705d53b9d08e88
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-08 13:02:37 -0500

    Add a create action for bands

--- app/controllers/api/v1/bands_controller.rb
+++ app/controllers/api/v1/bands_controller.rb
@@ -7,11 +7,18 @@ class Api::V1::BandsController < ApplicationController
 
   def create
     band = Band.new(band_params)
+    band.user = User.find(1)
+
+    if band.save
+      render json: band, status: 201, location: [:api, band]
+    else
+      render json: { errors: band.errors }, status: 422
+    end
   end
 
   protected
 
     def band_params
-      params.require(:band).permits(:name)
+      params.require(:band).permit(:name, :user)
     end
 end
