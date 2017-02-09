eb1fb2b8a0b55afb35b2f439f540dcdc663e4918
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-08 20:04:51 -0500

    Add lat and long to bands

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,6 +1,8 @@
 class Band < ApplicationRecord
   belongs_to :user
   acts_as_taggable_on :locations, :genres
+  geocoded_by :full_street_address   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
 
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208201331) do
+ActiveRecord::Schema.define(version: 20170209010412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bands", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.integer  "user_id"
+    t.string   "location",   default: "", null: false
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_bands_on_user_id", using: :btree
   end
 
