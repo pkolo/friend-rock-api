78687627e0b251b593e4d0c2bec58de26d7f7ae5
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-10 07:29:18 -0500

    Refactor unique pair validation

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,20 @@ class Relationship < ApplicationRecord
   belongs_to :band_one, class_name: Band
   belongs_to :band_two, class_name: Band
   belongs_to :action_band, class_name: Band
+
+  validate :unique_pair
+
+  def unique_pair
+    if any_pair? || any_reverse_pair?
+      errors.add(:band_one, "duplicate relationship exists")
+    end
+  end
+
+  def any_pair?
+    Relationship.where(band_one: band_one, band_two: band_two).any?
+  end
+
+  def any_reverse_pair?
+    Relationship.where(band_one: band_two, band_two: band_one).any?
+  end
 end
