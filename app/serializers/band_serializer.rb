46a7d80b552c60dbab9d7dab581deda0a7475305
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-10 11:58:36 -0500

    Add friends to serializer

--- app/serializers/band_serializer.rb
+++ app/serializers/band_serializer.rb
@@ -1,10 +1,21 @@
 class BandSerializer < ActiveModel::Serializer
-  attributes :id, :name, :genres, :location
+  attributes :id, :name, :genres, :location, :friendships
 
   def genres
     object.genres.map do |genre|
       genre.name
     end
+  end
+
+
+  def friendships
+    object.friendships.map do |friendship|
+      other_band_in friendship
+    end
+  end
+
+  def other_band_in(friendship)
+    object == friendship.band_one ? friendship.band_two : friendship.band_one
   end
 
 end
